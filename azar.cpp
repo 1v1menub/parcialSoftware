17159135293281ca5a9223074ed0cc3d7a5c769f
1v1menub <ignacio.rubio@utec.edu.pe>
2022-05-25 04:06:36 -0500

    TERMINADO (FUERA DE TIEMPO)

--- azar.cpp
+++ azar.cpp
@@ -5,54 +5,6 @@
 
 using namespace std;
 
-class Jugador {
-public:
-    EstrategiaJuego* estrategia;
-    void ElegirEstrategia(EstrategiaJuego* e) {
-        estrategia = e;
-    }
-
-    bool RecibirNumero(int n) {
-        estrategia->AgregarNumero(n);
-        bool g = estrategia->CalcularVictoria();
-        return g;
-    }
-};
-
-class Juego {
-private:
-    vector<Jugador*> jugadores;
-    vector<int> numeros;
-    bool terminado;
-    Juego();
-public:
-    Juego(const Juego&) = delete;
-
-    static Juego& ConseguirInstancia() {
-        static Juego instancia;
-        return instancia;
-    }
-
-    void GenerarNumero(int seedmod) {
-        srand(time(0) + seedmod);
-        int num = rand() % 100 + 1;
-        NotificarJugadores(num);
-    }
-
-    void NotificarJugadores(int n) {
-        for(auto &jug : jugadores) {
-            if(jug->RecibirNumero(n)) {
-                terminado = true;
-                AnunciarGanador(jug);
-            }
-        }
-    }
-
-    bool AnunciarGanador(Jugador* jug) {
-        cout << "El jugador con la estrategia" << jug->estrategia->nombre << endl << " ha ganado" << endl;
-    }
-};
-
 class EstrategiaJuego {
 public:
     vector<int> numeros;
@@ -70,6 +22,7 @@ class CincoPares : public EstrategiaJuego {
 public:
     CincoPares(vector<int> n) {
         numeros = n;
+        nombre = "Cinco Pares";
     }
 
     bool CalcularVictoria() {
@@ -93,6 +46,7 @@ class CincoImpares : public EstrategiaJuego {
 public:
     CincoImpares (vector<int> n) {
         numeros = n;
+        nombre = "Cinco Impares";
     }
 
     bool CalcularVictoria() {
@@ -116,6 +70,7 @@ class NumeroPrimo : public EstrategiaJuego {
 public:
     NumeroPrimo (vector<int> n) {
         numeros = n;
+        nombre = "Numero Primo";
     }
 
     bool CalcularVictoria() {
@@ -137,6 +92,7 @@ class TresMultiplos10 : public EstrategiaJuego {
 public:
     TresMultiplos10 (vector<int> n) {
         numeros = n;
+        nombre = "Tres Multiplos de 10";
     }
 
     bool CalcularVictoria() {
@@ -160,6 +116,7 @@ class DosMultiplos25 : public EstrategiaJuego {
 public:
     DosMultiplos25 (vector<int> n) {
         numeros = n;
+        nombre = "Dos Multiplos de 25";
     }
 
     bool CalcularVictoria() {
@@ -179,14 +136,111 @@ public:
     }
 };
 
+class Jugador {
+public:
+    EstrategiaJuego* estrategia;
 
-
-int main() {
-
-    Juego& j = Juego::ConseguirInstancia();
-    for(int i = 0 ; i < 5; i++ ) {
-        
+    void ElegirEstrategia(EstrategiaJuego* e) {
+        estrategia = e;
     }
 
+    bool RecibirNumero(int n) {
+        estrategia->AgregarNumero(n);
+        bool g = estrategia->CalcularVictoria();
+        return g;
+    }
+};
 
-}
+class Juego {
+private:
+    vector<Jugador*> jugadores;
+    vector<int> numeros;
+    bool terminado;
+    Juego() {
+        terminado = false;
+    }
+public:
+    Juego(const Juego&) = delete;
+
+    static Juego& ConseguirInstancia() {
+        static Juego instancia;
+        return instancia;
+    }
+
+    void GenerarNumero(int seedmod) {
+        srand(time(0) + seedmod);
+        int num = rand() % 100 + 1;
+        for(int i = 0; i < numeros.size(); i++) {
+            if(num == numeros[i]) {
+                GenerarNumero(seedmod + 100);
+                return;
+            }
+        }
+        numeros.push_back(num);
+        NotificarJugadores(num);
+    }
+
+    void NotificarJugadores(int n) {
+        for(auto &jug : jugadores) {
+            if(jug->RecibirNumero(n)) {
+                terminado = true;
+                AnunciarGanador(jug);
+            }
+        }
+    }
+
+    void AnunciarGanador(Jugador* jug) {
+        cout << "El jugador con la estrategia " << jug->estrategia->nombre << " ha ganado" << endl;
+    }
+
+    bool ConseguirEstado() {
+        return terminado;
+    }
+
+    vector<int> ConseguirVector() {
+        return numeros;
+    }
+
+    bool InscribirJugador(Jugador* ju) {
+        bool usado = false;
+        for(auto &jug : jugadores) {
+            if(ju->estrategia->nombre == jug->estrategia->nombre) {
+                usado = true;
+            }
+        }
+        if(!usado) {
+            jugadores.push_back(ju);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+};
+
+int main() {
+    Juego& j = Juego::ConseguirInstancia();
+    cout << "El juego se abre" << endl;
+    Jugador jug1;
+    jug1.ElegirEstrategia(new CincoPares(j.ConseguirVector()));
+    j.InscribirJugador(&jug1);
+    Jugador jug2;
+    jug2.ElegirEstrategia(new CincoImpares(j.ConseguirVector()));
+    j.InscribirJugador(&jug2);
+    Jugador jug3;
+    jug3.ElegirEstrategia(new NumeroPrimo(j.ConseguirVector()));
+    j.InscribirJugador(&jug3);
+    Jugador jug4;
+    jug4.ElegirEstrategia(new TresMultiplos10(j.ConseguirVector()));
+    j.InscribirJugador(&jug4);
+    Jugador jug5;
+    jug5.ElegirEstrategia(new DosMultiplos25(j.ConseguirVector()));
+    j.InscribirJugador(&jug5);
+    while(!j.ConseguirEstado()){
+        int ctr = 0;
+        cout << "El juego continua" << endl;
+        j.GenerarNumero(ctr);
+        ctr++;
+    }
+}; 
